c14ba7b3ca9e4a27c3f651944b5fd406ecb0f680
Aarón Lopez <119780016+AaronLopez12@users.noreply.github.com>
2024-05-09 09:46:14 -0600

    Add files via upload

---
+++ instaladorR.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install python3-pip
+
+pip install pyqt5
+
+sudo apt-get install gedit git
+
+git clone https://github.com/AaronLopez12/IGD-IRACE
+
+cd IGD-IRACE
+
+pwd
+
+R
+
+install.packages("irace")
